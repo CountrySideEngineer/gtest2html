5bcf19a6f9c2dd3b8e737f4cad1e9a114fe7fbde
CountrySideEngineer <deep-sleeper@nifty.com>
2021-02-21 22:43:29 +0900

    Update application code and design.

--- dev/dev/gtest2html/Program.cs
+++ dev/dev/gtest2html/Program.cs
@@ -10,80 +10,73 @@ namespace gtest2html
 {
 	class Program
 	{
-		private static void XmlToHtml(string xmlFilePath)
-		{
-			var xmlFileName = Path.GetFileName(xmlFilePath);
-			var htmlFileName = Path.ChangeExtension(xmlFileName, "html");
-
-			using (var xmlStream = new FileStream(xmlFilePath, FileMode.Open))
-			{
-				var serializer = new XmlSerializer(typeof(TestSuites));
-				var testSuites = (TestSuites)serializer.Deserialize(xmlStream);
-				var htmlTemplate = new TestSuiteHtmlTemplate(testSuites);
-				var content = htmlTemplate.TransformText();
-				using (var textStream = new StreamWriter(htmlFileName, false, Encoding.GetEncoding("UTF-8")))
-				{
-					textStream.Write(content);
-				}
-			}
-		}
-
-		private static void CreateIndexHtml(IEnumerable<string> xmlFiles)
-		{
-			var testSuiteList = new List<TestSuites>();
-			foreach (var xmlFilePath in xmlFiles)
-			{
-				using (var xmlStream = new FileStream(xmlFilePath, FileMode.Open))
-				{
-					var serializer = new XmlSerializer(typeof(TestSuites));
-					var testSuites = (TestSuites)serializer.Deserialize(xmlStream);
-					testSuites.TestName = Path.GetFileNameWithoutExtension(xmlFilePath);
-					testSuiteList.Add(testSuites);
-				}
-			}
-			var indexTemplate = new IndexHtmlTemplate(testSuiteList);
-			var content = indexTemplate.TransformText();
-
-			using (var textStream = new StreamWriter(@".\index.html", false, Encoding.GetEncoding("UTF-8")))
-			{
-				textStream.Write(content);
-			}
-
-		}
-
 		static void Main(string[] args)
 		{
-			/*
-			 *	Judge the arguement means file or directory.
-			 *	If directory, read xml file path from it, otherwise if file, file convert into html.
-			 */
-			if (args.Count() < 1)
+			if (args.Count() < 2)
 			{
-				Console.WriteLine("Invalid argument.");
+				Console.WriteLine("2 arguments are needed.");
+				Console.WriteLine("1st : Path to output.");
+				Console.WriteLine("2nd : Path to directory contains the test result xml files.");
 
 				return;
 			}
 
-			var filePathList = new List<string>();
-			if (File.Exists(args[0]))
+			string outputDirPath = args[0];
+			string xmlDirPath = args[1];
+
+			var outputDirInfo = CreateOutputDirInfo(outputDirPath);
+			var xmlFileInfos = GetXmlFilePaths(xmlDirPath);
+			var xmlFileList = GetXmlFilePaths(xmlDirPath);
+			var toHtml = new XmlToHtml(outputDirInfo, xmlFileList);
+			toHtml.Convert();
+
+			return;
+		}
+
+		/// <summary>
+		/// Returns the list of file information contained in direcotry specified by argument
+		/// dirPath.
+		/// </summary>
+		/// <param name="dirPath">Path to directory contains result of test in XML format.</param>
+		/// <returns>List of test result XML file information.</returns>
+		private static IEnumerable<FileInfo> GetXmlFilePaths(string dirPath)
+		{
+			if (File.Exists(dirPath))
 			{
-				filePathList.Add(args[0]);
+				throw new ArgumentException("The path is not directory.");
 			}
-			else if (Directory.Exists(args[0]))
+			else if (!Directory.Exists(dirPath))
 			{
-				var directoryInfo = new DirectoryInfo(args[0]);
-				FileInfo[] files = directoryInfo.GetFiles("*.xml");
-				foreach (var file in files)
-				{
-					filePathList.Add(file.FullName);
-				}
+				throw new ArgumentException("The directory has not existed.");
+			}
+			else
+			{
+				var dirInfo = new DirectoryInfo(dirPath);
+				var fileInfos = dirInfo.GetFiles("*.xml").ToList();
+
+				return fileInfos;
+			}
+		}
+
+		/// <summary>
+		/// Create directory information object about output directory.
+		/// </summary>
+		/// <param name="dirPath">Path to output html result.</param>
+		/// <returns>Directory information of output directory.</returns>
+		private static DirectoryInfo CreateOutputDirInfo(string dirPath)
+		{
+			//If the path specified by argument dirPath mean file, it is invalid.
+			if (File.Exists(dirPath)) {
+				throw new ArgumentException("The path has already existed.");
 			}
 
-			foreach (var xmlFile in filePathList)
+			if (!Directory.Exists(dirPath))
 			{
-				XmlToHtml(xmlFile);
+				Directory.CreateDirectory(dirPath);
 			}
-			CreateIndexHtml(filePathList);
+
+			var dirInfo = new DirectoryInfo(dirPath);
+			return dirInfo;
 		}
 	}
 }
---
+++ dev/dev/gtest2html/XmlToHtml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace gtest2html
+{
+	public class XmlToHtml
+	{
+		#region Public Properties
+		/// <summary>
+		///	Output directory information.
+		/// </summary>
+		public DirectoryInfo OutputDirInfo;
+
+		/// <summary>
+		/// List of test resutl xml files.
+		/// </summary>
+		public IEnumerable<FileInfo> XmlFileInfos;
+		#endregion
+
+		#region Constructor and destructor(Finalizer)
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="outputDir">Path to output report.</param>
+		/// <param name="xmlFilePathList">List of file path to result of test in xml format.</param>
+		public XmlToHtml(DirectoryInfo outputDir, IEnumerable<FileInfo> xmlFileInfos)
+		{
+			this.OutputDirInfo = outputDir;
+			this.XmlFileInfos = xmlFileInfos;
+		}
+		#endregion
+
+		#region Other methods and private properties in calling order
+		/// <summary>
+		/// Run convert.
+		/// </summary>
+		public void Convert()
+		{
+			this.ConvertXmlToHtml();
+			this.CreateIndexHtml();
+		}
+
+		/// <summary>
+		/// Convert xml into html.
+		/// </summary>
+		protected void ConvertXmlToHtml()
+		{
+			foreach (var xmlFileInfoItem in this.XmlFileInfos)
+			{
+				this.ConvertXmlToHtml(xmlFileInfoItem);
+
+			}
+		}
+
+		/// <summary>
+		/// Convert xml specified by argumetn xmlFilePaht.
+		/// </summary>
+		/// <param name="xmlFilePath">Path to xml file to be converted into </param>
+		protected void ConvertXmlToHtml(FileInfo fileInfo)
+		{
+			//入力ファイル名を元に、出力ファイルのパスを特定する。
+			var inputFileName = Path.GetFileName(fileInfo.FullName);
+			var outputFileName = Path.ChangeExtension(inputFileName, "html");
+			var outputFileInfo = new FileInfo(this.OutputDirInfo.FullName + @"\" + outputFileName);
+
+			using (var xmlReader = new StreamReader(fileInfo.FullName, Encoding.GetEncoding("UTF-8")))
+			{
+				var serializer = new XmlSerializer(typeof(TestSuites));
+				var testSuites = (TestSuites)serializer.Deserialize(xmlReader);
+				var htmlTemplate = new TestSuiteHtmlTemplate(testSuites);
+				var content = htmlTemplate.TransformText();
+				using (var htmlStream = new StreamWriter(outputFileInfo.FullName, false, Encoding.GetEncoding("UTF-8")))
+				{
+					htmlStream.Write(content);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Create index.html file.
+		/// </summary>
+		protected void CreateIndexHtml()
+		{
+			var testSuiteList = new List<TestSuites>();
+			foreach (var xmlFileInfo in this.XmlFileInfos)
+			{
+				using (var xmlStream = new StreamReader(xmlFileInfo.FullName, Encoding.GetEncoding("UTF-8")))
+				{
+					var serializer = new XmlSerializer(typeof(TestSuites));
+					var testSuites = (TestSuites)serializer.Deserialize(xmlStream);
+					var testSuiteName = Path.GetFileNameWithoutExtension(xmlFileInfo.FullName);
+					testSuites.TestName = testSuiteName;
+					testSuiteList.Add(testSuites);
+				}
+			}
+			var indexHtmlFilePath = this.OutputDirInfo.FullName + @"\index.html";
+			var indexTemplate = new IndexHtmlTemplate(testSuiteList);
+			var content = indexTemplate.TransformText();
+			using (var htmlStream = new StreamWriter(indexHtmlFilePath, false, Encoding.GetEncoding("UTF-8")))
+			{
+				htmlStream.Write(content);
+			}
+		}
+		#endregion
+	}
+}
