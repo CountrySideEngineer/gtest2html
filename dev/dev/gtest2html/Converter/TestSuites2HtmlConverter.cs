1d36503e12704b5341bc877b30e6aa6e19b83050
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-19 05:06:31 +0900

    Add converter to convert TestSuite object into HTML data.

---
+++ dev/dev/gtest2html/Converter/TestSuites2HtmlConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Converter
+{
+	class TestSuites2HtmlConverter : IConverter<IEnumerable<TestSuite>, string>
+	{
+		public string Convert(IEnumerable<TestSuite> src)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
