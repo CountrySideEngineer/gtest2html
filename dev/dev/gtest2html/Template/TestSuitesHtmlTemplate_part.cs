811fc903007750903c77eb31303d0f584632416c
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-19 05:19:00 +0900

    Add template for TestSuites HTML file.

---
+++ dev/dev/gtest2html/Template/TestSuitesHtmlTemplate_part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Template
+{
+	public partial class TestSuitesHtmlTemplate
+	{
+		public TestSuites TestSuites { get; set; }
+
+		public TestSuitesHtmlTemplate(TestSuites testSuites)
+		{
+			TestSuites = testSuites;
+		}
+	}
+}
