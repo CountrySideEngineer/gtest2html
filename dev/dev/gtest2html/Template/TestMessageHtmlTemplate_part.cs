9725144df7b34817875b119cbfb975ec358d457c
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-21 08:44:32 +0900

    Add template about failure message.

---
+++ dev/dev/gtest2html/Template/TestMessageHtmlTemplate_part.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Template
+{
+	partial class TestMessageHtmlTemplate
+	{
+		public string parentPage { get; set; }
+		public Failure Failure { get; set; }
+
+		public TestMessageHtmlTemplate(string parentPage, Failure failure)
+		{
+			this.parentPage = parentPage;
+			this.Failure = failure;
+		}
+	}
+}
