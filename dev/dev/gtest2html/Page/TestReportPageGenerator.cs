3b9daa1874ea8dbefe339738f0f972ddd6417494
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-21 18:17:24 +0900

    Add classes to generate HTML page file. Add class to generate report page file in HTML format. With this change, update project file to contain the newly added files.

---
+++ dev/dev/gtest2html/Page/TestReportPageGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Page
+{
+	using gtest2html.Converter;
+
+	class TestReportPageGenerator : AReportPageFileGenerator
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestReportPageGenerator() : base() { }
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="outputRoot">Output root directory information.</param>
+		public TestReportPageGenerator(DirectoryInfo outputRoot) : base(outputRoot) { }
+
+		public override void Generate(IEnumerable<FileInfo> fileInfos)
+		{
+			foreach (var fileInfo in fileInfos)
+			{
+				TestSuites testSuites = base.ExtractTestSuites(fileInfo);
+				string content = Suites2Content(testSuites);
+				SendReport(fileInfo, content);
+			}
+		}
+
+		public virtual void Generate(FileInfo fileInfo)
+		{
+			TestSuites testSuites = base.ExtractTestSuites(fileInfo);
+			string content = Suites2Content(testSuites);
+			SendReport(fileInfo, content);
+		}
+
+		protected virtual FileInfo GetOutputFileInfo(FileInfo fileInfo)
+		{
+			string fileName = Path.GetFileNameWithoutExtension(fileInfo.FullName);
+			string outputFileName = $@"{OutputRoot.FullName}\{fileName}.html";
+			FileInfo outputFileInfo = new FileInfo(outputFileName);
+
+			return outputFileInfo;
+		}
+
+		protected virtual string Suites2Content(TestSuites suites)
+		{
+			var template = new TestSuites2HtmlConverter();
+			string content = template.Convert(suites);
+			return content;
+		}
+
+		protected void SendReport(FileInfo fileInfo, string content)
+		{
+			FileInfo destInfo = GetOutputFileInfo(fileInfo);
+
+			using (var writer = new StreamWriter(destInfo.FullName, false, Encoding.UTF8))
+			{
+				writer.Write(content);
+			}
+		}
+	}
+}
