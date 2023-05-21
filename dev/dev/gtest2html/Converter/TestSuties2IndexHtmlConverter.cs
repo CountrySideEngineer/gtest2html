579cc79b47a045544507e58f2b1d08205481e915
CountrySideEngineer <deep-sleeper@nifty.com>
2023-05-21 14:35:12 +0900

    Add template and converter for index.html.

---
+++ dev/dev/gtest2html/Converter/TestSuties2IndexHtmlConverter.cs
@@ -0,0 +1,23 @@
+﻿using gtest2html.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Converter
+{
+	class TestSuties2IndexHtmlConverter : ATest2HtmlConveter<IEnumerable<TestSuites>>
+	{
+		/// <summary>
+		/// Create template for index.html
+		/// </summary>
+		/// <param name="src"></param>
+		/// <returns>Template for index.html</returns>
+		protected override TemplateCommonBase GetTemplate(IEnumerable<TestSuites> src)
+		{
+			var template = new TestSuites2IndexHtmlTemplate(src);
+			return template;
+		}
+	}
+}
