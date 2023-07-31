44b66bcf771f5e4bf9cd2d847aa62695fa531927
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-01 05:54:39 +0900

    Update solution to develop library, dll, to convert xml to html.

---
+++ dev/dev/libgtest2html/Converter/File/TestXml2Html.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using gtest2html;
+
+namespace gtest2html.Converter.File
+{
+	internal class TestXml2Html : IXml2Html<FileInfo>
+	{
+		public DirectoryInfo OutputDir { get; protected set; }
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestXml2Html()
+		{
+			string location = Assembly.GetEntryAssembly().Location;
+			OutputDir = System.IO.Directory.GetParent(location);
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="outputDir">Output directory information.</param>
+		public TestXml2Html(DirectoryInfo outputDir)
+		{
+			OutputDir = outputDir;
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="sources">Path to directory to output.</param>
+		public TestXml2Html(string outputDir)
+		{
+			OutputDir = new DirectoryInfo(outputDir);
+		}
+
+		public void Convert(IEnumerable<FileInfo> sources)
+		{
+			foreach	(var source in sources)
+			{
+				Convert(source);
+			}
+		}
+
+		public void Convert(FileInfo source)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ dev/dev/libgtest2html/IXml2Html.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace gtest2html
+{
+	interface IXml2Html<S>
+	{
+		void Convert(IEnumerable<S> sources);
+
+		void Convert(S source);
+	}
+}
