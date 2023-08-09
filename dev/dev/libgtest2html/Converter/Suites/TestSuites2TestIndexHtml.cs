00f2b99ad665f190b1c7ed391886b2260e87494c
CountrySideEngineer <deep-sleeper@nifty.com>
2023-08-09 13:05:47 +0900

    Add classes to convert TestSuites class into HTML files.

---
+++ dev/dev/libgtest2html/Converter/Suites/TestSuites2TestIndexHtml.cs
@@ -0,0 +1,26 @@
+﻿using gtest2html.Page.Html.Generator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Converter.Suites
+{
+	internal class TestSuites2TestIndexHtml : ATestSuites2Html
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestSuites2TestIndexHtml() : base() { }
+
+		public TestSuites2TestIndexHtml(DirectoryInfo dirInfo) : base(dirInfo) { }
+
+		public override void Convert(TestSuites testSuites)
+		{
+			var generator = new TestSuitesIndexFileGenerator(OutputDir);
+			generator.Generate(testSuites);
+		}
+	}
+}
---
+++ dev/dev/libgtest2html/Converter/Suites/TestSuites2TestMessageHtml.cs
@@ -0,0 +1,26 @@
+﻿using gtest2html.Page.Html.Generator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Converter.Suites
+{
+	internal class TestSuites2TestMessageHtml : ATestSuites2Html
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestSuites2TestMessageHtml() : base() { }
+
+		public TestSuites2TestMessageHtml(DirectoryInfo dirInfo) : base(dirInfo) { }
+
+		public override void Convert(TestSuites testSuites)
+		{
+			var generator = new TestMessageFileGenerator(OutputDir);
+			generator.Generate(testSuites);
+		}
+	}
+}
