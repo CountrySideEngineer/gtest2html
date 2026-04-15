067f8737cf0281bffdc6bca1dc901f912f97e1a1
CountrySideEngineer <deep-sleeper@nifty.com>
2026-04-15 22:10:54 +0900

    Add CSS file generator. Add classes to generate css file and its content in string.

---
+++ dev/dev/dotnet/libgtest2html/Page/Css/Generator/CssFileGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gtest2html.Page.Css.Generator
+{
+    public class CssFileGenerator : CssGenerator
+    {
+        public static string FileName = "report";
+        public static string FileNameWithExtention = $"{FileName}.css";
+
+        public override string Generate(DirectoryInfo outputDir)
+        {
+            string content = base.Generate(outputDir);
+
+            Generate(content, outputDir);
+            return content;
+        }
+
+        protected virtual void Generate(string content, DirectoryInfo outputDir)
+        {
+            string filePath = $@"{outputDir.FullName}\{FileNameWithExtention}";
+            using (var stream = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                stream.Write(content);
+            }
+        }
+    }
+}
---
+++ dev/dev/dotnet/libgtest2html/Page/Css/Generator/CssGenerator.cs
@@ -0,0 +1,28 @@
+﻿using gtest2html.Page.Css.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace gtest2html.Page.Css.Generator
+{
+    public class CssGenerator : IPageGenerator<DirectoryInfo, string>
+    {
+        public CssGenerator() { }
+
+        public virtual string Generate(DirectoryInfo src)
+        {
+            var cssTemplate = new CssTemplate();
+            var content = cssTemplate.TransformText();
+
+            return content;
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
